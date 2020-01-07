eba8c5de9089b48c7a1048bd9d8f4719711517d6
tom hannaford <thom.hannaford@gmail.com>
2020-01-07 02:13:36 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,7 +13,7 @@ def directors_totals(nds)
     puts nds
 end
 
-directors_totals(directors_box_office)
+directors_totals(directors_database)
 
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
